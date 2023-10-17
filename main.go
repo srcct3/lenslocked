d44c5c0dd2dc0bfc1b4075a1ff5c1c24da59d007
eliasyeme <eliasyeme@gmail.com>
2023-10-17 09:46:27 +0300

    Add controller for parsing template before startup

--- main.go
+++ main.go
@@ -2,49 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/eliasyeme/lenslocked/controllers"
 	"github.com/eliasyeme/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
 
-func excuteTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	t, err := views.Parse(filepath)
-
-	if err != nil {
-		log.Printf("Error parsing template: %v", err)
-		http.Error(w, "Error parsing template", http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	path := filepath.Join("templates", "home.tmpl")
-	excuteTemplate(w, path)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	path := filepath.Join("templates", "contact.tmpl")
-	excuteTemplate(w, path)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	path := filepath.Join("templates", "faq.tmpl")
-	excuteTemplate(w, path)
-}
-
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	t, err := views.Parse(filepath.Join("templates", "home.tmpl"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(t))
+
+	t, err = views.Parse(filepath.Join("templates", "contact.tmpl"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(t))
+
+	t, err = views.Parse(filepath.Join("templates", "faq.tmpl"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(t))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
